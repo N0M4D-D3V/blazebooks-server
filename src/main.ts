f15cd6bf5c51a8146b8a62ea69ea4a94df968257
N0M4D <victor@gg-victor.dev>
2024-02-09 22:37:34 +0100

    cors config and return json example response

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+const allowedOrigins: string[] = ["http://localhost:4200"];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1) callback(null, true);
+      else callback(new Error("Not allowed by cors"));
+    },
+  });
   await app.listen(3000);
 }
+
 bootstrap();
