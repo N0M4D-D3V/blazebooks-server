c5179e16e65a00b2f8fedf5d127179982f877802
N0M4D <victor@gg-victor.dev>
2024-02-13 21:26:17 +0100

    core moved to modules folder - imports updated as well

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
-import { DatabaseModule } from "./core/database/database.module";
+import { DatabaseModule } from "@core/database/database.module";
 import { ConfigModule } from "@nestjs/config";
-import { UsersModule } from "./modules/users/users.module";
-import { BookModule } from "./modules/book/book.module";
+import { UsersModule } from "@modules/users/users.module";
+import { BookModule } from "@modules/book/book.module";
 
 @Module({
   imports: [
