1413e85d1774b1e7bfc3d9c4091f6db7ebdc0fc1
N0M4D <victor@gg-victor.dev>
2024-02-13 21:12:44 +0100

    user module and service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { ControllerModule } from "./controllers/controller.module";
 import { DatabaseModule } from "./core/database/database.module";
 import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     ControllerModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
