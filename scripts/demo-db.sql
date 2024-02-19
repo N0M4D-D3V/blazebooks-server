40dba0984b49d07ef92e4012d44c893b8aa8304f
N0M4D <victor@gg-victor.dev>
2024-02-19 21:55:48 +0100

    demo db script

---
+++ scripts/demo-db.sql
@@ -0,0 +1,18 @@
+-- Database: blazebooks_dev
+
+DROP DATABASE IF EXISTS blazebooks_dev;
+CREATE DATABASE blazebooks_dev
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+-- Query 1: Insert the books into the 'Books' table
+INSERT INTO public."Books" (id, title, subtitle, author, description, "imgUrl", "createdAt", "updatedAt")
+VALUES
+  (0, '1984', 'Conoce al Gran Hermano', 'George Orwell', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum finibus nisl id interdum. Curabitur viverra, lectus at porttitor scelerisque, augue quam tincidunt tellus, vel varius nulla mauris ut ex. Ut sem leo, laoreet in leo at, facilisis cursus odio. Nullam ac sapien felis. Sed hendrerit quis erat egestas sollicitudin. Curabitur sit amet volutpat elit. Ut at enim accumsan, laoreet ex sed, vehicula nibh. Mauris sapien elit, auctor tincidunt condimentum in, tristique vitae eros. Proin cursus vestibulum urna non tincidunt. Suspendisse eget maximus ante.', 'assets/books/1984/cover.jpg', '19/02/2024', '19/02/2024'),
+  (1, 'Madrid Zombie', '¡Rambo no!', 'Bruno Piqué', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum finibus nisl id interdum. Curabitur viverra, lectus at porttitor scelerisque, augue quam tincidunt tellus, vel varius nulla mauris ut ex. Ut sem leo, laoreet in leo at, facilisis cursus odio. Nullam ac sapien felis. Sed hendrerit quis erat egestas sollicitudin. Curabitur sit amet volutpat elit. Ut at enim accumsan, laoreet ex sed, vehicula nibh. Mauris sapien elit, auctor tincidunt condimentum in, tristique vitae eros. Proin cursus vestibulum urna non tincidunt. Suspendisse eget maximus ante.', 'assets/books/madridzombie/cover.jpg', '19/02/2024', '19/02/2024');
